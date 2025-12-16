8f1e84eb78da3a573ab1a68bfa973dd273afbb9c
fifthmarauder <eeshashahbaz12345@gmail.com>
2025-12-16 18:31:57 +0500

    Chore: completed featured products

--- src/components/FeaturedProducts/FeaturedProducts.tsx
+++ src/components/FeaturedProducts/FeaturedProducts.tsx
@@ -1,4 +1,4 @@
-import { ShoppingCart, Sparkles } from "lucide-react";
+import { Heart, ShoppingCart, Sparkles } from "lucide-react";
 import Button from "../common/Button/Button";
 import styles from "./featuredproducts.module.css";
 import Wave from "../common/Wave";
@@ -9,7 +9,7 @@ const FeaturedProducts = () => {
       <div className={styles.main}>
         <div className={styles.container}>
           <div className={styles.intro}>
-            <div className="heading">
+            <div className="heading" style={{ color: "var(--color-darkPink)" }}>
               FEAT
               <span style={{ fontFamily: "var(--font-knotnoodle" }}>U</span>RED
               PR<span style={{ fontFamily: "var(--font-knotnoodle" }}>O</span>
@@ -30,7 +30,6 @@ const FeaturedProducts = () => {
                   />
                   <div className={styles.productInfo}>
                     <div className={styles.productType}>Plushies</div>
-                    <div className={styles.productDescription}>15 in stock</div>
                   </div>
                   <div className={styles.productName}>HUNTRIX FULL SET</div>
                   <div className={styles.productDescription}>
@@ -52,11 +51,33 @@ const FeaturedProducts = () => {
           <Button
             text="View All Products"
             Icon={Sparkles}
-            containerStyles={{ flexDirection: "row-reverse" }}
+            containerStyles={{
+              flexDirection: "row-reverse",
+              marginBottom: "64px",
+            }}
           />
+          <div className={styles.exploreShopContainer}>
+            <div className={styles.exploreHeading}>
+              ST<span style={{ fontFamily: "var(--font-knotnoodle)" }}>A</span>
+              RT YO
+              <span style={{ fontFamily: "var(--font-knotnoodle)" }}>U</span>R
+              CR<span style={{ fontFamily: "var(--font-knotnoodle)" }}>O</span>
+              CHET JOURN
+              <span style={{ fontFamily: "var(--font-knotnoodle)" }}>E</span>Y T
+              <span style={{ fontFamily: "var(--font-knotnoodle)" }}>O</span>DAY
+            </div>
+            <div className={styles.para}>
+              Whether you're looking for a unique gift or treating yourself, our
+              handmade crochet items bring warmth and joy to any space
+            </div>
+            <Button text="Explore Our Shop" Icon={Heart} />
+          </div>
         </div>
       </div>
-      <Wave color="var(--color-lightPink)" />
+      <div style={{ backgroundColor: "var(--color-surfaceBlack)" }}>
+        {" "}
+        <Wave color="var(--color-lightPink)" />
+      </div>
     </>
   );
 };
