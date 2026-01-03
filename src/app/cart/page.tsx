eddec473b9d3a438006dbfc75eebb86362c8f1f3
fifthmarauder <eeshashahbaz12345@gmail.com>
2026-01-03 19:15:11 +0500

    Chore: Initial Cart

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,22 @@
+import { ArrowLeft } from "lucide-react";
+import styles from "./cart.module.css";
+
+const Cart = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.backButton}>
+          <ArrowLeft />
+          Continue Shopping
+        </div>
+        <div className="heading" style={{ color: "var(--color-darkPink)" }}>
+          SH<span style={{ fontFamily: "var(--font-knotnoodle)" }}>O</span>PPING
+          C<span style={{ fontFamily: "var(--font-knotnoodle)" }}>A</span>RT
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
